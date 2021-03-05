ffc33484a472d879c3ba58b80c648c47ccaffd57
ericlarslee <ericlarslee@gmail.com>
2021-03-05 15:53:17 -0600

    this update includes a functional random selector from array and gives option to choose a 2nd option if first option was not satisfactory

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,14 +1,38 @@
-'use strict'
 
-let destination = ["International Falls", "Duluth", "Fort Francis", "Fargo", "Grand Marais"];
-let restaurant = ["Dairy King", "MacDons", "A&W"];
-let transport = ["car", "local Amish taxi-service", "plane"];
-let entertainment = ["go hiking", "go shopping", "visit friends", "view local church architecture"];
 
-function randomSelect(entry) {
- return entry[Math.floor(Math.random() * entry.length)];
+let cities = ["International Falls", "Duluth", "Fort Francis", "Fargo", "Grand Marais"];
+
+function randomSelect(entry, nameOfEntry) {
+    let randomNumber = Math.floor(Math.random() * entry.length);
+    let option = entry[randomNumber];
+    let decision = prompt(`How do you feel about ${option} as the option for ${nameOfEntry} for the day trip? Answer "yes" or "no"`);
+        if(decision !== "yes"){
+            randomNumber = Math.floor(Math.random() * entry.length);
+            return entry[randomNumber];
+        }
+        else {
+            return option;
+        }
+        
 }
-console.log(randomSelect(destination));
-console.log(randomSelect(restaurant));
-console.log(randomSelect(transport));
-console.log(randomSelect(entertainment));
+
+console.log(randomSelect(cities, "cities"));
+
+
+/* function acceptRandomSelect() {
+    let decision = prompt(`Are you alright with going to ${city}`)
+} */
+
+function letUsGoOnADayTrip() {
+
+    let cities = ["International Falls", "Duluth", "Fort Francis", "Fargo", "Grand Marais"];
+    let restaurantOptions = ["Dairy King", "MacDons", "A&W"];
+    let transportOptions = ["car", "local Amish taxi-service", "plane"];
+    let entertainmentOptions = ["go hiking", "go shopping", "visit friends", "view local church architecture"];
+
+    let city = randomSelect(cities, "cities");
+    let restaurant = randomSelect(restaurantOptions);
+    let transport = randomSelect(transportOptions);
+    let entertainment = randomSelect(entertainmentOptions);
+
+}
